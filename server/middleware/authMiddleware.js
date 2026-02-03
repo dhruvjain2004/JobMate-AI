7d725503a0a6e49cc7c8b58e58a5044052527206
dhruvjain2004 <dhruvjain527@gmail.com>
2026-02-03 14:36:01 +0530

    FIX

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -15,6 +15,7 @@ export const protectCompany = async (req, res, next) => {
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.company = await Company.findById(decoded.id).select("-password");
+    req.userId = decoded.id;
     next();
   } catch (error) {
     res.json({ success: false, message: error.message });
